d917e951f61d32778d617a6a7b823535c4fc3c56
daze <grigpavel21@gmail.com>
2023-10-12 19:30:33 +0300

    Add bank and dealership application factories

---
+++ database/factories/DealershipApplicationFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Bank;
+use App\Models\DealershipApplication;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<DealershipApplication>
+ */
+class DealershipApplicationFactory extends Factory
+{
+    protected $model = DealershipApplication::class;
+
+    public function definition(): array
+    {
+        $status = $this->faker->randomElement(['new', 'in_progress', 'approved', 'rejected']);
+        $bankId = Bank::inRandomOrder()->first()->id;
+
+        return [
+            'bank_id' => $bankId,
+            'dealer_name' => $this->faker->company,
+            'contact_person' => $this->faker->name,
+            'loan_amount' => $this->faker->numberBetween(5000, 50000),
+            'loan_term' => $this->faker->numberBetween(6, 60),
+            'interest_rate' => $this->faker->randomFloat(2, 3, 10),
+            'reason' => $this->faker->sentence,
+            'status' => $status,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Bank;
+use App\Models\DealershipApplication;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // Create 10 banks
+        Bank::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Create 50 applications
+        DealershipApplication::factory(50)->create();
     }
 }
